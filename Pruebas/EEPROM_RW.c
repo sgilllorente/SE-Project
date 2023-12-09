11aa6d41a5159f2cf3aba53e79dff9d47b8e31bc
mikealitor <55632677+mikealitor@users.noreply.github.com>
2023-12-09 11:21:17 +0100

    Add files via upload

---
+++ Pruebas/EEPROM_RW.c
@@ -0,0 +1,47 @@
+#pragma config PLLDIV = 2, CPUDIV = OSC1_PLL2, USBDIV = 2
+#pragma config FOSC = HSPLL_HS, FCMEN = OFF, IESO = OFF
+#pragma config PWRT = OFF, BOR = OFF, BORV = 3, VREGEN = OFF
+#pragma config WDT = OFF
+#pragma config WDTPS = 32768
+#pragma config CCP2MX = ON, PBADEN = OFF, LPT1OSC = OFF, MCLRE = ON
+#pragma config STVREN = ON, LVP = OFF, ICPRT = OFF, XINST = OFF
+#pragma config CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF
+#pragma config CPB = OFF, CPD = OFF
+#pragma config WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF
+#pragma config WRTC = OFF, WRTB = OFF, WRTD = OFF
+#pragma config EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF
+#pragma config EBTRB = OFF
+
+#define _XTAL_FREQ 48000000
+#include <xc.h>
+#include <stdio.h>
+
+#include "eeprom.h"                     // Libreria de la memoria eeprom interna
+#include "uart.h"                       // Libreria del modulo USART
+
+char buffer[20];
+
+void main()
+{
+    ADCON1bits.PCFG = 0x0F;             // Coloca todos los pines como digitales
+    Uart_Init(9600);                    // Inicializa la comunicacion serial a 9600 baudios
+    
+    Uart_Send_String("Escribiendo datos...\r\n");
+    for(char i=0; i<10; i++)
+    {
+        EEPROM_Write(i, i+1);
+    }
+    __delay_ms(1000);
+    
+    while(1)
+    {
+        for(char i=0; i<10; i++)
+        {
+            unsigned char dato = EEPROM_Read(i);
+            sprintf(buffer, "Dato: %u , Direccion: %u\r\n", dato, i);
+            Uart_Send_String(buffer);
+            __delay_ms(100);
+        }
+        Uart_Send_String("\r\n");
+    }
+}
